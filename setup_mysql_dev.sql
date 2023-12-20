fe5bd465b88c1ccad0801348071f699ceba0c4dc
mouad-suuu <moad.menniui@gmail.com>
2023-12-20 20:28:04 +0100

    the database hbnb dev db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,12 @@
-CREATE DATABASE IF NOT EXISTS  hbnb_dev_db;
+-- Create or use the database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd' ;
+-- Create or use the user hbnb_dev
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
+-- Grant privileges to the user hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
-GRANT SELECT ON performance_schema.* To 'hbnb_dev'@'localhost';
-
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
