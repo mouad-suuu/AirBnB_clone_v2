2ef19470e0f3aec55efbb4117cae9c171dfa77d0
mouad-suuu <moad.menniui@gmail.com>
2023-12-20 23:29:29 +0100

    storage file

--- models/place.py
+++ models/place.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """This is the place class"""
 from sqlalchemy.ext.declarative import declarative_base
-from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 import models
-import shlex
 
 
 place_amenity = Table("place_amenity", Base.metadata,
