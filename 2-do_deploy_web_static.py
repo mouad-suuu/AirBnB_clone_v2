68b0c4856c01784ecce61786702b5672b19246fb
mouad mennioui <moad.menniui@gmail.com>
2024-01-11 13:01:26 +0100

    task 2 fixing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["100.25.183.90", "54.144.83.63"]
 env.user = "ubuntu"
 
 
