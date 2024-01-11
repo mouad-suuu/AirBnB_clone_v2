29cb41850a5aaf7730cdd54d3d905547a84429fe
mouad-suuu <moad.menniui@gmail.com>
2024-01-11 03:47:41 -0800

    task 2(tgz file) fixing

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-
+"""A module for Fabric script that generates a .tgz archive."""
 import os
 from datetime import datetime
-from fabric.api import local, runs_once, task
+from fabric.api import local, runs_once
+
 
 @runs_once
 def do_pack():
-    """
-    Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack
-    """
+    """Archives the static files."""
     if not os.path.isdir("versions"):
         os.mkdir("versions")
-    the_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(the_time.year, the_time.month, the_time.day, the_time.hour, the_time.minute, the_time.second)
+    d_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        d_time.year,
+        d_time.month,
+        d_time.day,
+        d_time.hour,
+        d_time.minute,
+        d_time.second
+    )
     try:
         print("Packing web_static to {}".format(output))
         local("tar -cvzf {} web_static".format(output))
         size = os.stat(output).st_size
         print("web_static packed: {} -> {} Bytes".format(output, size))
-        return output
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
-
-if __name__ == "__main__":
-    do_pack()
-
+    except Exception:
+        output = None
+    return output
