42a06e690708acc20e16a7d9e990378dae48b8f7
mouad-suuu <moad.menniui@gmail.com>
2024-01-27 18:01:28 +0100

    task 4

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,11 +24,13 @@ def c_route(text):
     """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_route(text='is cool'):
     """display “python ” followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
