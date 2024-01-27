336912cc4fb3b876b46b3dda856f5d9ebcc06008
mouad-suuu <moad.menniui@gmail.com>
2024-01-27 08:15:02 -0800

    task 3

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,28 +5,29 @@ adding the C rout and <text>
 
 from flask import Flask
 
-app = flask(__name__)
+app = Flask(__name__)
 
-@app.route('/',strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index():
     """
     returning Hello HBNB!
     """
     return 'Hello HBNB!'
 
-@app.route('/hbnb',strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
     returning HBNB!
     """
     return 'HBNB'
 
-@app.route('/c/<text>',strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def Cis(text):
     """
     returning what is in the <text>
     """
-    return 'C' + text.replace('_', ' ' )
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
+
