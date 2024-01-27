1237ac3b95b123c3431c258aef2f1e58b9db84d1
mouad-suuu <moad.menniui@gmail.com>
2024-01-27 17:55:56 +0100

    task 4

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,18 +6,15 @@ starts a Flask web application
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
-
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
-
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
